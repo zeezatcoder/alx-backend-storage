ac04586c53d0ec964be1c0d29d8f0f6132c673d0
zeezatcoder <azeezatmaja@gmail.com>
2023-04-06 12:23:26 +0000

    task complete

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- Write a SQL script that lists all bands, main style, ranked by their longevity
+-- Write a SQL script that lists all bands_name, and  longevity
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0

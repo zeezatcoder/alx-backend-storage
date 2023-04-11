5f229d2ee72856f4ae0db9aa2ba3b30d455b9402
zeezatcoder <azeezatmaja@gmail.com>
2023-04-11 09:00:43 -0700

    task update

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -10,4 +10,4 @@ def insert_school(mongo_collection, **kwargs):
     insert a documents into a collection
     """
     data = mongo_collection.insert_one(kwargs)
-    return data.
+    return data.inserted_id
